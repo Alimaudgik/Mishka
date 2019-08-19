ea70cc5122bba6de3f16bd2d814e735f2252c829
Arseni <arseni_p@tut.by>
2019-08-19 17:09:13 +0300

    main-nav 2/3

---
+++ source/js/menu.js
@@ -0,0 +1,34 @@
+var navMain = document.querySelector('.main-nav__list');
+var navToggle = document.querySelector('.main-nav__toggle');
+var close = document.querySelector('.close')
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('site-list-closed')) {
+    navMain.classList.remove('site-list-closed');
+    navMain.classList.add('site-list-opened');
+  };
+
+  if (navToggle.classList.contains('toggle-on')) {
+    navToggle.classList.remove('toggle-on');
+    navToggle.classList.add('toggle-off');
+    
+    close.classList.remove('close-off');
+    close.classList.add('close-on')
+  } else {
+    navToggle.classList.remove('toggle-off');
+    navToggle.classList.add('toggle-on');
+  }
+});
+
+close.addEventListener('click', function() {
+  if (close.classList.contains('close-on')) {
+    close.classList.remove('close-on');
+    close.classList.add('close-off');
+
+    navToggle.classList.remove('toggle-off');
+    navToggle.classList.add('toggle-on');
+
+    navMain.classList.add('site-list-closed');
+    navMain.classList.remove('site-list-opened');
+  }
+})
