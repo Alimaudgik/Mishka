f4a1fb91be5b7bf6c587d4a8392b03dc3c2660f2
Alimaudgik <arseni_p@tut.by>
2019-09-26 23:13:41 +0300

    fin

--- source/js/cart-menu.js
+++ source/js/cart-menu.js
@@ -1,12 +1,12 @@
 var openOrder = document.querySelector('.order-button');
 var cartBlock = document.querySelector('.cart__block');
 var whiteback = document.querySelector('.whiteback');
-var closeOrder = document.querySelector('.close-order')
+var closeOrder = document.querySelector('.close-order');
 
-openOrder.addEventListener('onclick', function() {
+
+openOrder.addEventListener('click', function() {
   if (cartBlock.classList.contains('cart__block-off')) {
     cartBlock.classList.remove('cart__block-off');
-    cartBlock.classList.add('cart__block-on')
+    cartBlock.classList.add('cart__block-on');
   };
-})
-
+});
